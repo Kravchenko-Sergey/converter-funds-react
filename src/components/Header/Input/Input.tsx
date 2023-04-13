80fd34b4077c413bfbe0aa6a8c1325516100f0ec
Kravchenko-Sergey <sergey.ose.pyatigorsk@gmail.com>
2023-04-13 22:52:19 +0300

    moved the handler logic to a higher level

--- src/components/Header/Input/Input.tsx
+++ src/components/Header/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import searchIcon from '../../../assets/images/search-magnifying-glass-svgrepo-com.svg'
 import { selectError, selectInputValue } from '../../../state/selectors'
@@ -6,37 +6,35 @@ import { addFundAC, inputErrorsAC } from '../../../state/actions'
 import { Button, Input2, InputBlock, Error } from './StyledInput'
 import { onChangeInputValueAC, resetInputValueAC } from '../../../state/actions'
 
-type InputPropsType = {
-	totalValue: number
-}
+type InputPropsType = { totalValue: number }
 
 export const Input = ({ totalValue }: InputPropsType) => {
 	const inputValue = useSelector(selectInputValue)
 	const error = useSelector(selectError)
 	const dispatch = useDispatch()
+
+	const onChangeHandler = (e: any) => {
+		dispatch(onChangeInputValueAC(e.currentTarget.value))
+	}
+
+	const onKeyPressHandler = (e: any) => {
+		if (e.charCode === 13) {
+			dispatch(addFundAC(inputValue, totalValue))
+			dispatch(inputErrorsAC(inputValue))
+			dispatch(resetInputValueAC())
+		}
+	}
+
+	const onClickHandler = () => {
+		dispatch(addFundAC(inputValue, totalValue))
+		dispatch(inputErrorsAC(inputValue))
+		dispatch(resetInputValueAC())
+	}
+
 	return (
 		<InputBlock>
-			<Input2
-				type='text'
-				value={inputValue}
-				onChange={e => {
-					dispatch(onChangeInputValueAC(e.currentTarget.value))
-				}}
-				onKeyPress={(e: any) => {
-					if (e.charCode === 13) {
-						dispatch(addFundAC(inputValue, totalValue))
-						dispatch(inputErrorsAC(inputValue))
-						dispatch(resetInputValueAC())
-					}
-				}}
-			/>
-			<Button
-				onClick={() => {
-					dispatch(addFundAC(inputValue, totalValue))
-					dispatch(inputErrorsAC(inputValue))
-					dispatch(resetInputValueAC())
-				}}
-			>
+			<Input2 value={inputValue} onChange={onChangeHandler} onKeyPress={onKeyPressHandler} />
+			<Button onClick={onClickHandler}>
 				<img src={searchIcon} alt='searchIcon' />
 			</Button>
 			<Error>{error}</Error>
